47bb52c3c5621eb74c0b04d416d26394a760aa19
LongliZheng <lzheng228@connect.hkust-gz.edu.cn>
2026-05-09 20:20:32 +0800

    feat: build construction graphs from splits

--- src/graph.rs
+++ src/graph.rs
@@ -1,5 +1,6 @@
 use crate::repr::{Rational, TensorDef, Term};
 use crate::split::{Split, SplitInterface};
+use std::collections::HashMap;
 
 const MAX_TERMS: usize = 64;
 
@@ -30,7 +31,91 @@ pub fn build_graphs_from_splits(
     splits_by_term: &[Vec<Split>],
 ) -> Result<Vec<ConstrGraph>, GraphError> {
     validate_splits_by_term(def, splits_by_term)?;
-    Ok(vec![])
+
+    let mut graphs: HashMap<SplitInterface, ConstrGraph> = HashMap::new();
+
+    for (term_idx, splits) in splits_by_term.iter().enumerate() {
+        let source_coeff = &def.terms[term_idx].coeff;
+
+        for split in splits {
+            let graph = graphs
+                .entry(split.interface.clone())
+                .or_insert_with(|| empty_graph(split.interface.clone()));
+            insert_split(graph, source_coeff, term_idx, split)?;
+        }
+    }
+
+    Ok(finalize_graphs(graphs))
+}
+
+fn empty_graph(interface: SplitInterface) -> ConstrGraph {
+    ConstrGraph {
+        interface,
+        left_nodes: vec![],
+        right_nodes: vec![],
+        edges: vec![],
+    }
+}
+
+fn insert_split(
+    graph: &mut ConstrGraph,
+    source_coeff: &Rational,
+    term_idx: usize,
+    split: &Split,
+) -> Result<(), GraphError> {
+    let (left, right, coeff) = normalize_edge_contribution(source_coeff, split);
+    let left_id = ensure_node(&mut graph.left_nodes, left);
+    let right_id = ensure_node(&mut graph.right_nodes, right);
+    push_edge(&mut graph.edges, left_id, right_id, term_idx, coeff)
+}
+
+fn normalize_edge_contribution(source_coeff: &Rational, split: &Split) -> (Term, Term, Rational) {
+    let mut left = split.left.clone();
+    let mut right = split.right.clone();
+    let coeff = source_coeff * &left.coeff * &right.coeff;
+    left.coeff = Rational::new(1, 1);
+    right.coeff = Rational::new(1, 1);
+    (left, right, coeff)
+}
+
+fn ensure_node(nodes: &mut Vec<Term>, term: Term) -> usize {
+    if let Some(index) = nodes.iter().position(|node| node == &term) {
+        index
+    } else {
+        let index = nodes.len();
+        nodes.push(term);
+        index
+    }
+}
+
+fn push_edge(
+    edges: &mut Vec<GraphEdge>,
+    left_id: usize,
+    right_id: usize,
+    term_idx: usize,
+    coeff: Rational,
+) -> Result<(), GraphError> {
+    let term_bit = 1_u64 << term_idx;
+
+    edges.push(GraphEdge {
+        left_id,
+        right_id,
+        coeff,
+        terms_used: term_bit,
+    });
+    Ok(())
+}
+
+fn finalize_graphs(graphs: HashMap<SplitInterface, ConstrGraph>) -> Vec<ConstrGraph> {
+    graphs
+        .into_values()
+        .filter_map(|mut graph| {
+            graph
+                .edges
+                .retain(|edge| edge.coeff != Rational::new(0, 1));
+            (graph.edges.len() >= 2).then_some(graph)
+        })
+        .collect()
 }
 
 fn validate_splits_by_term(
--- tests/graph.rs
+++ tests/graph.rs
@@ -1,5 +1,8 @@
-use gristmill_symbolics::graph::{build_graphs_from_splits, GraphError};
-use gristmill_symbolics::repr::{Rational, TensorDef, TensorId, Term};
+use gristmill_symbolics::graph::{build_graphs_from_splits, ConstrGraph, GraphEdge, GraphError};
+use gristmill_symbolics::repr::{
+    Factor, Index, IndexId, RangeId, Rational, TensorDef, TensorId, Term,
+};
+use gristmill_symbolics::split::{Split, SplitInterface};
 
 fn one() -> Rational {
     Rational::new(1, 1)
@@ -16,6 +19,48 @@ fn empty_def_with_terms(len: usize) -> TensorDef {
                 factors: vec![],
             })
             .collect(),
+    }
+}
+
+fn idx(id: u32, range: u32) -> Index {
+    Index {
+        id: IndexId(id),
+        range: RangeId(range),
+    }
+}
+
+fn factor(tensor: u32, indices: &[u32]) -> Factor {
+    Factor {
+        tensor: TensorId(tensor),
+        indices: indices.iter().copied().map(IndexId).collect(),
+    }
+}
+
+fn term(coeff: Rational, factors: Vec<Factor>) -> Term {
+    Term {
+        coeff,
+        sum_indices: vec![],
+        factors,
+    }
+}
+
+fn split(left: Term, right: Term, interface: SplitInterface) -> Split {
+    Split {
+        left,
+        right,
+        interface,
+    }
+}
+
+fn iface(
+    left_external: Vec<Index>,
+    right_external: Vec<Index>,
+    contracted: Vec<Index>,
+) -> SplitInterface {
+    SplitInterface {
+        left_external,
+        right_external,
+        contracted,
     }
 }
 
@@ -41,4 +86,112 @@ fn more_than_64_terms_returns_graph_error() {
         build_graphs_from_splits(&def, &splits_by_term),
         Err(GraphError::TooManyTerms { len: 65, max: 64 })
     );
+}
+
+#[test]
+fn builds_graph_bucket_with_independent_left_and_right_nodes() {
+    let a = idx(0, 0);
+    let b = idx(1, 0);
+    let k = idx(2, 0);
+    let interface = iface(vec![a], vec![b], vec![k]);
+    let shared_term_value = term(one(), vec![factor(7, &[0, 2])]);
+    let source_def = TensorDef {
+        base: TensorId(9),
+        ext_indices: vec![a, b],
+        terms: vec![
+            Term {
+                coeff: Rational::new(3, 1),
+                sum_indices: vec![k],
+                factors: vec![],
+            },
+            Term {
+                coeff: Rational::new(5, 1),
+                sum_indices: vec![k],
+                factors: vec![],
+            },
+        ],
+    };
+    let splits_by_term = vec![
+        vec![split(
+            shared_term_value.clone(),
+            shared_term_value.clone(),
+            interface.clone(),
+        )],
+        vec![split(
+            term(one(), vec![factor(8, &[2, 1])]),
+            term(one(), vec![factor(10, &[0])]),
+            interface.clone(),
+        )],
+    ];
+
+    let graphs = build_graphs_from_splits(&source_def, &splits_by_term).unwrap();
+
+    assert_eq!(
+        graphs,
+        vec![ConstrGraph {
+            interface,
+            left_nodes: vec![
+                shared_term_value.clone(),
+                term(one(), vec![factor(8, &[2, 1])]),
+            ],
+            right_nodes: vec![
+                shared_term_value,
+                term(one(), vec![factor(10, &[0])]),
+            ],
+            edges: vec![
+                GraphEdge {
+                    left_id: 0,
+                    right_id: 0,
+                    coeff: Rational::new(3, 1),
+                    terms_used: 1,
+                },
+                GraphEdge {
+                    left_id: 1,
+                    right_id: 1,
+                    coeff: Rational::new(5, 1),
+                    terms_used: 2,
+                },
+            ],
+        }]
+    );
+}
+
+#[test]
+fn edge_coefficients_absorb_source_and_side_coefficients_and_nodes_are_unit_terms() {
+    let interface = iface(vec![], vec![], vec![]);
+    let def = TensorDef {
+        base: TensorId(0),
+        ext_indices: vec![],
+        terms: vec![
+            Term {
+                coeff: Rational::new(6, 1),
+                sum_indices: vec![],
+                factors: vec![],
+            },
+            Term {
+                coeff: one(),
+                sum_indices: vec![],
+                factors: vec![],
+            },
+        ],
+    };
+    let splits_by_term = vec![
+        vec![split(
+            term(Rational::new(2, 1), vec![factor(0, &[0])]),
+            term(Rational::new(-3, 1), vec![factor(1, &[1])]),
+            interface.clone(),
+        )],
+        vec![split(
+            term(one(), vec![factor(2, &[2])]),
+            term(one(), vec![factor(3, &[3])]),
+            interface.clone(),
+        )],
+    ];
+
+    let graphs = build_graphs_from_splits(&def, &splits_by_term).unwrap();
+    let graph = &graphs[0];
+
+    assert_eq!(graph.left_nodes[0].coeff, one());
+    assert_eq!(graph.right_nodes[0].coeff, one());
+    assert_eq!(graph.edges[0].coeff, Rational::new(-36, 1));
 }
