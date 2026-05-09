5b9e68115a3ade78901d67b0a1cf857c3cf9a54c
LongliZheng <lzheng228@connect.hkust-gz.edu.cn>
2026-05-09 13:52:41 +0800

    test: cover split interfaces and side subterms

--- tests/split.rs
+++ tests/split.rs
@@ -98,3 +98,92 @@ fn two_factor_term_produces_one_unit_coefficient_split() {
         }]
     );
 }
+
+#[test]
+fn three_factor_chain_emits_public_unordered_bipartitions() {
+    let range = RangeId(0);
+    let a = Index {
+        id: IndexId(10),
+        range,
+    };
+    let b = Index {
+        id: IndexId(0),
+        range,
+    };
+    let c = Index {
+        id: IndexId(30),
+        range,
+    };
+    let d = Index {
+        id: IndexId(20),
+        range,
+    };
+    let x = TensorId(0);
+    let y = TensorId(1);
+    let z = TensorId(2);
+
+    let def = TensorDef {
+        base: TensorId(3),
+        ext_indices: vec![a, b],
+        terms: vec![],
+    };
+    let term = Term {
+        coeff: Rational::new(-5, 2),
+        sum_indices: vec![d, c],
+        factors: vec![
+            Factor {
+                tensor: x,
+                indices: vec![a.id, c.id],
+            },
+            Factor {
+                tensor: y,
+                indices: vec![c.id, d.id],
+            },
+            Factor {
+                tensor: z,
+                indices: vec![d.id, b.id],
+            },
+        ],
+    };
+
+    let splits = enumerate_splits(&term, &def).unwrap();
+    assert_eq!(splits.len(), 3);
+
+    assert_eq!(splits[0].interface.left_external, vec![a]);
+    assert_eq!(splits[0].interface.right_external, vec![b]);
+    assert_eq!(splits[0].interface.contracted, vec![c]);
+    assert_eq!(splits[0].left.factors, vec![term.factors[0].clone()]);
+    assert_eq!(
+        splits[0].right.factors,
+        vec![term.factors[1].clone(), term.factors[2].clone()]
+    );
+    assert_eq!(splits[0].left.sum_indices, vec![]);
+    assert_eq!(splits[0].right.sum_indices, vec![d]);
+
+    assert_eq!(splits[1].interface.left_external, vec![]);
+    assert_eq!(splits[1].interface.right_external, vec![b, a]);
+    assert_eq!(splits[1].interface.contracted, vec![d, c]);
+    assert_eq!(splits[1].left.factors, vec![term.factors[1].clone()]);
+    assert_eq!(
+        splits[1].right.factors,
+        vec![term.factors[0].clone(), term.factors[2].clone()]
+    );
+    assert_eq!(splits[1].left.sum_indices, vec![]);
+    assert_eq!(splits[1].right.sum_indices, vec![]);
+
+    assert_eq!(splits[2].interface.left_external, vec![a]);
+    assert_eq!(splits[2].interface.right_external, vec![b]);
+    assert_eq!(splits[2].interface.contracted, vec![d]);
+    assert_eq!(
+        splits[2].left.factors,
+        vec![term.factors[0].clone(), term.factors[1].clone()]
+    );
+    assert_eq!(splits[2].right.factors, vec![term.factors[2].clone()]);
+    assert_eq!(splits[2].left.sum_indices, vec![c]);
+    assert_eq!(splits[2].right.sum_indices, vec![]);
+
+    for split in splits {
+        assert_eq!(split.left.coeff, one());
+        assert_eq!(split.right.coeff, one());
+    }
+}
