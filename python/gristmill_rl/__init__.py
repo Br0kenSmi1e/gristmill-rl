bdcf14c5f41b0690b9e755c28725d6c76dab5e48
LongliZheng <lzheng228@connect.hkust-gz.edu.cn>
2026-06-09 16:29:12 +0800

    refactor: remove deprecated reinforce prototypes

--- python/gristmill_rl/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Reinforcement-learning prototype for gristmill-symbolics rewrites."""
-
-__version__ = "0.1.0"
--- python/tests/test_rl_package.py
+++
@@ -1,5 +0,0 @@
-def test_rl_package_exports_version():
-    import gristmill_rl
-
-    assert isinstance(gristmill_rl.__version__, str)
-    assert gristmill_rl.__version__ == "0.1.0"
