7007ad0bceba6bf8b2ff008c376fa43705691a3f
LongliZheng <lzheng228@connect.hkust-gz.edu.cn>
2026-06-11 11:39:11 +0800

    refactor: split rewrite facade and scalar module

---
+++ src/rewrite/row.rs
@@ -0,0 +1,14 @@
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RewriteStateRow;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ActionSpaceRow;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum ActionSpaceEntry {}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ValidatedActionRow;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum ValidatedActionEntry {}
