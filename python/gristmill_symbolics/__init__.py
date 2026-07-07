e446e11c9da6541633f72414be96e9c2feefafc2
LongliZheng <lzheng228@connect.hkust-gz.edu.cn>
2026-07-07 11:07:05 +0800

    test: remove stale python ml tests

---
+++ python/gristmill_symbolics/__init__.py
@@ -0,0 +1,27 @@
+"""Python bindings for the gristmill-symbolics rewrite kernel."""
+
+from ._core import (
+    ActionSpace,
+    GristmillSymbolicsError,
+    TensorComputation,
+    action_space_for_def,
+    action_spaces_for_batch,
+    apply_decision,
+    apply_decisions_for_batch,
+    equivalent_computations,
+    validate_decision,
+    validate_decisions_for_batch,
+)
+
+__all__ = (
+    "ActionSpace",
+    "GristmillSymbolicsError",
+    "TensorComputation",
+    "action_space_for_def",
+    "action_spaces_for_batch",
+    "apply_decision",
+    "apply_decisions_for_batch",
+    "equivalent_computations",
+    "validate_decision",
+    "validate_decisions_for_batch",
+)
--- python/tests/direct_optimizer/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tests for the direct optimizer package."""
